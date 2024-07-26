69a53c0c8c713cde7e657221d3f5b18471f00305
my3code <princeadilehou@gmail.com>
2024-07-26 05:45:20 +0100

    auth.php

--- authentificate.php
+++ authentificate.php
@@ -1,23 +1,38 @@
 <?php
 session_start();
 
-$default_username = 'citech';
-$default_password = 'Soutenance1234';
+// Identifiants admin
+$default_username_admin = 'citech';
+$default_password_admin = 'Soutenance1234';
+// Identifiants docteur
+$default_username_doctor = 'citech';
+$default_password_doctor = 'Soutenance567';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if ($username === $default_username && $password === $default_password) {
+    // Vérification des identifiants admin
+    if ($username === $default_username_admin && $password === $default_password_admin) {
         $_SESSION['username'] = $username;
         header('Location: dashbord.php');
         exit();
-    } else {
+    }
+    // Vérification des identifiants docteur
+    elseif ($username === $default_username_doctor && $password === $default_password_doctor) {
+        $_SESSION['username'] = $username;
+        header('Location: rendezvous.php');
+        exit();
+    } 
+    else {
+        // Redirection avec message d'erreur
         header('Location: login.php?error=1');
         exit();
     }
-} else {
-    header('Location: index.php');
+} 
+else {
+    // Redirection si la requête n'est pas POST
+    header('Location: login.php');
     exit();
 }
 ?>
